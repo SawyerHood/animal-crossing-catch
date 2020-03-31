cf50523ca46c06193075790b1b1b8d0eb08cbcb5
Sawyer Hood <sawyerjhood@gmail.com>
2020-03-30 21:15:59 -0700

    Add bugs

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -53,9 +53,63 @@ async function loadFish() {
 
     return JSON.stringify(Object.values(resultMap));
   });
-  
+
   console.log(result);
   await browser.close();
 }
 
-loadFish();
+async function loadBugs() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(BUG_URL);
+
+  const result = await page.evaluate(() => {
+    const resultMap = {};
+    const bodies = Array.from(
+      document.querySelectorAll(".sortable tbody")
+    ).map(body => Array.from(body.children, r => Array.from(r.children)));
+    for (row of bodies[0]) {
+      const name = row[0].textContent.trim();
+      const imgChild = row[1].children[0];
+      const url = imgChild ? row[1].children[0].href : null;
+      const sellPrice = +row[2].textContent.trim().replace(",", "");
+      const location = row[3].textContent.trim();
+      const time = row[4].textContent.trim();
+      const months = row.slice(5).map(cell => cell.textContent.trim() !== "-");
+
+      const key = name.toLowerCase();
+      resultMap[key] = {
+        name,
+        imageURL: url,
+        sellPrice,
+        location,
+        time,
+        months,
+      };
+    }
+
+    for (row of bodies[1]) {
+      const name = row[0].textContent.trim();
+      const shTime = row[4].textContent.trim();
+      const shMonths = row
+        .slice(5)
+        .map(cell => cell.textContent.trim() !== "-");
+      const key = name.toLowerCase();
+      const res = resultMap[key];
+      if (res) {
+        res.shTime = shTime;
+        res.shMonths = shMonths;
+      }
+    }
+
+    return JSON.stringify(Object.values(resultMap));
+  });
+
+  console.log(result);
+  await browser.close();
+}
+
+module.exports = {
+  loadBugs,
+  loadFish,
+}
